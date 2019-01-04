c3493e1d1eeb936122d729eee542a0197c0cba76
Junio C Hamano <gitster@pobox.com>
2019-01-04 12:35:20 -0800

    Simplify generation of po4a.conf
    There is no need to use a subshell for redirection; just redirect the output from the "cat" command directly.

--- create_po4a_conf.sh
+++ create_po4a_conf.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-(cat <<EOF
+cat <<EOF >po4a.conf
 [po4a_langs] fr
 [po4a_paths] po/documentation.pot \$lang:po/documentation.\$lang.po
 [options] opt: " -k 80"
 
 EOF
-) >po4a.conf 
 
 for f in $(cat sources.txt)
 do
